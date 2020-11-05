925fbb5709e4c69a4a492f4a3857fabf8c8b0376
Szymon Rykała <szymonrykala@gmail.com>
2020-11-05 22:48:59 +0100

    split to policies

--- models/User.php
+++ models/User.php
@@ -14,8 +14,7 @@ final class User extends GenericModel
             'type' => MyInt::class,
         ],
         'access_id' => [
-            'type' => MyInt::class,
-            'default' => 1
+            'type' => MyInt::class
         ],
         'name' => [
             'type' => MyString::class,
@@ -44,7 +43,6 @@ final class User extends GenericModel
             'type' => MyString::class,
             'create' => true,
             'update' => true,
-            'pattern' => '/.+{5,}/',
             'sanitize' => FILTER_SANITIZE_EMAIL,
             'validate' => FILTER_VALIDATE_EMAIL
         ],
@@ -70,35 +68,25 @@ final class User extends GenericModel
         ]
     ];
 
-    private string $email;
-    public array $data;
-
-    public function setEmail(string $email): void
-    {
-        $this->email = filter_var($email, FILTER_SANITIZE_EMAIL);
-        $this->data = $this->read(['email' => $this->email])[0];
-    }
-
-
-    public function login($password): void
+    public function login(string $email, string $password): void
     {
         /**
          * Login the user
          */
-        // list(
-        // 'password' => $userPassword,
-        // 'id' => $userID,
-        // 'access_id' => $accessID,
-        // 'login_fails' => $loginFails,
-        // 'activated' => $activated
-        // )
-        $this->setID($this->data['id']);
+
+        // $this->setID($this->data['id']);
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            throw new HttpBadRequestException('Given email has incorrect format.');
+        }
+
+        $this->data = $this->read(['email' => $email])[0];
+        $this->id = $this->data['id'];
 
         if ($this->data['login_fails'] >= 5) {
             throw new HttpForbiddenException('Can not login. Login failed to many times and Your account is locked. Please contact with Your administrator');
         }
 
-        if ((bool)$this->data['activated'] === false) {
+        if ($this->data['activated'] === false) {
             throw new HttpConflictException("Can not authenticate because user is not activated");
         }
 
@@ -111,8 +99,16 @@ final class User extends GenericModel
 
     public function register(array $data): int
     {
+        // checking password policies
+        $this->fieldCreatePolicy('password', $data);
 
+        // hashing the password
         $data['password'] = password_hash($data['password'], PASSWORD_BCRYPT, ['cost' => 12]);
+
+        // unset checking password with regex pattern becouse of it has been already checked 
+        // now it is a hash so it can not be validated with defined rules
+        unset($this->SCHEMA['password']['pattern']);
+
         $this->create($data);
         return $this->DB->lastInsertID();
     }
